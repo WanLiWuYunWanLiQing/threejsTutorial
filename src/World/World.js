2d1cd113506e5aecd040876aa5d7fa6849b1dfdf
zhanghualei <zhanghualei@agilebot.com.cn>
2024-03-14 16:39:45 +0800

    first commit

---
+++ src/World/World.js
@@ -0,0 +1,33 @@
+import { createCamera } from './components/camera.js';
+import { createCube } from './components/cube.js';
+import { createScene } from './components/scene.js';
+
+import { createRenderer } from './systems/renderer.js';
+import { Resizer } from './systems/Resizer.js';
+
+let camera;
+let renderer;
+let scene;
+
+class World {
+    constructor(container) {
+        camera = createCamera();
+        scene = createScene();
+        renderer = createRenderer();
+        container.append(renderer.domElement);
+
+        const cube = createCube();
+
+        scene.add(cube);
+
+        const resizer = new Resizer(container, camera, renderer);
+    }
+
+    // 2. Render the scene
+    render() {
+        // draw a single frame
+        renderer.render(scene, camera);
+    }
+}
+
+export { World };
