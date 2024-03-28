c19bedcbb7143994683248d9f7de6478edfb46db
zhanghualei <zhanghualei@agilebot.com.cn>
2024-03-28 19:45:46 +0800

    texture

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -1,11 +1,25 @@
-import { BoxGeometry, Mesh, MeshStandardMaterial, MathUtils } from 'three';
+import { BoxGeometry, Mesh, MeshStandardMaterial, MathUtils, TextureLoader } from 'three';
+
+function createMaterial() {
+    // create a texture loader
+    const textureLoader = new TextureLoader();
+
+    // load a texture
+    const texture = textureLoader.load("/assets/textures/uv-test-bw.png");
+
+    // create a "standard" material
+    const material = new MeshStandardMaterial({ map: texture });
+
+    return material;
+}
+
 
 function createCube() {
     // create a geometry
     const geometry = new BoxGeometry(2, 2, 2);
 
     // create a physically correct "standard" material
-    const material = new MeshStandardMaterial({ color: "purple" });
+    const material = createMaterial();
 
     // create a Mesh containing the geometry and material
     const cube = new Mesh(geometry, material);
