793cbf9cc2d96c3b317d3544ccf2a3ff97080838
zhanghualei <zhanghualei@agilebot.com.cn>
2024-03-28 19:10:41 +0800

    prepare for textrue

--- src/World/World.js
+++ src/World/World.js
@@ -1,24 +1,31 @@
 import { createCamera } from './components/camera.js';
 import { createCube } from './components/cube.js';
+import { createLights } from './components/lights.js';
 import { createScene } from './components/scene.js';
 
 import { createRenderer } from './systems/renderer.js';
 import { Resizer } from './systems/Resizer.js';
+import { Loop } from './systems/Loop.js';
 
 let camera;
 let renderer;
 let scene;
+let loop;
 
 class World {
     constructor(container) {
         camera = createCamera();
         scene = createScene();
         renderer = createRenderer();
+        loop = new Loop(scene, camera, renderer);
         container.append(renderer.domElement);
 
         const cube = createCube();
+        const light = createLights();
 
-        scene.add(cube);
+        loop.updatables.push(cube);
+
+        scene.add(cube, light);
 
         const resizer = new Resizer(container, camera, renderer);
     }
@@ -27,6 +34,15 @@ class World {
     render() {
         // draw a single frame
         renderer.render(scene, camera);
+    }
+
+    start() {
+        loop.start();
+    }
+
+
+    stop() {
+        loop.stop();
     }
 }
 
--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -1,14 +1,26 @@
-import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three';
+import { BoxGeometry, Mesh, MeshStandardMaterial, MathUtils } from 'three';
 
 function createCube() {
     // create a geometry
     const geometry = new BoxGeometry(2, 2, 2);
 
-    // create a default (white) Basic material
-    const material = new MeshBasicMaterial();
+    // create a physically correct "standard" material
+    const material = new MeshStandardMaterial({ color: "purple" });
 
     // create a Mesh containing the geometry and material
     const cube = new Mesh(geometry, material);
+
+    cube.rotation.set(-0.5, -0.1, 0.8);
+
+    const radiansPerSecond = MathUtils.degToRad(30);
+
+    // this method will be called once per frame
+    cube.tick = (delta) => {
+        // increase the cube's rotation each frame
+        cube.rotation.x += radiansPerSecond * delta;
+        cube.rotation.y += radiansPerSecond * delta;
+        cube.rotation.z += radiansPerSecond * delta;
+    }
 
     return cube;
 }
