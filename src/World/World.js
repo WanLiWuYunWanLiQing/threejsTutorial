5e82fad4999b961af121801774300aeed8eb0ca1
zhanghualei <zhanghualei@agilebot.com.cn>
2024-04-01 14:51:06 +0800

    loader gltfmodels

--- src/World/World.js
+++ src/World/World.js
@@ -1,3 +1,4 @@
+import { loadBirds } from './components/birds/birds.js';
 import { createCamera } from './components/camera.js';
 import { createLights } from './components/lights.js';
 import { createScene } from './components/scene.js';
@@ -8,10 +9,9 @@ import { createRenderer } from './systems/renderer.js';
 import { Resizer } from './systems/Resizer.js';
 import { Loop } from './systems/Loop.js';
 
-import { Train } from '../../assets/models/train/Train.js';
-
 let camera;
 let renderer;
+let controls;
 let scene;
 let loop;
 
@@ -23,17 +23,21 @@ class World {
         loop = new Loop(scene, camera, renderer);
         container.append(renderer.domElement);
 
-        const controls = createControls(camera, renderer.domElement);
+        controls = createControls(camera, renderer.domElement);
         // 按需渲染 controls 的方法
         // controls.addEventListener('change', () => { this.render(); });
         const { mainLight, ambientLight } = createLights();
-        const train = new Train();
-
 
         loop.updatables.push(controls);
-        scene.add(ambientLight, mainLight, train);
+        scene.add(ambientLight, mainLight);
 
         const resizer = new Resizer(container, camera, renderer);
+    }
+
+    async init() {
+        const { parrot, flamingo, stork } = await loadBirds();
+        controls.target.copy(parrot.position);
+        scene.add(parrot, flamingo, stork);
     }
 
     // 2. Render the scene
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { World } from './World/World.js';
 
 // create the main function
-function main() {
+async function main() {
     // Get a reference to the container element
     const container = document.querySelector('#scene-container');
 
     // 1. Create an instance of the World app
     const world = new World(container);
+
+    // complete async tasks
+    await world.init();
 
     // produce a single frame (render on demand)
     world.render();
@@ -16,4 +19,6 @@ function main() {
 }
 
 // call main to start the app
-main();
+main().catch((err) => {
+    console.error('main() error:', err);
+});
